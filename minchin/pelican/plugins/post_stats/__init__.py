a82d7119bb6bb8aae5d3e42ae9349ece3e287c6a
MinchinWeb <w_minchin@hotmail.com>
2023-08-09 14:39:57 -0600

    black-ify code

--- minchin/pelican/plugins/post_stats/__init__.py
+++ minchin/pelican/plugins/post_stats/__init__.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 
 # Module Metadata
-__title__ = 'minchin.pelican.plugins.post_stats'
+__title__ = "minchin.pelican.plugins.post_stats"
 __version__ = "1.1.1"
-__description__ = 'This Pelcan plugin calculates various statistics about a post and stores them in an article.stats dictionary.'
-__author__ = 'William Minchin'
-__email__ = 'w_minchin@hotmail.com'
-__url__ = 'https://github.com/MinchinWeb/minchin.pelican.plugins.post_stats'
-__license__ = 'GNU Affero General Public License v3'
+__description__ = "This Pelcan plugin calculates various statistics about a post and stores them in an article.stats dictionary."
+__author__ = "William Minchin"
+__email__ = "w_minchin@hotmail.com"
+__url__ = "https://github.com/MinchinWeb/minchin.pelican.plugins.post_stats"
+__license__ = "GNU Affero General Public License v3"
 
 from .post_stats import *
